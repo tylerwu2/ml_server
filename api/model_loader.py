c2e64ac5d0fca8aa20e39403d6d3441a8a54d8fc
Tyler Wu <tyler_wu@berkeley.edu>
2026-06-09 14:59:30 -0700

    Built ModelLoader Structure

--- api/model_loader.py
+++ api/model_loader.py
@@ -1,3 +1,24 @@
 # load model from /model directory or other pipeline 
-import pathlib, joblib 
+import pathlib, joblib
 
+class ModelLoader():
+
+    def __init__(self, model_path):
+        self.model_path = model_path
+        self.model = None
+        self.is_loaded = False
+
+    def load(self):
+        self.model = joblib.load(self.model_path)
+        self.is_loaded = True 
+        print("Model Loaded.")
+
+    def predict(self, text):
+        label = self.model.predict(text)
+
+        return {
+            "label" : label,
+        }
+
+    
+        
